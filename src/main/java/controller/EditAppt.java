3b99ba06f287ae436c21a51aec4d92afc5609691
LabUser <sjime88@wgu.edu>
2023-02-10 17:05:49 -0700

    EditAppt working a lot better on display

--- src/main/java/controller/EditAppt.java
+++ src/main/java/controller/EditAppt.java
@@ -1,11 +1,10 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import model.Appointment;
 
@@ -32,10 +31,10 @@ public class EditAppt {
     private DatePicker end_date;
 
     @FXML
-    private Spinner<?> endtimehour;
+    private ComboBox<String> endHour;
 
     @FXML
-    private Spinner<?> endtimemin;
+    private ComboBox<String> endMinute;
 
     @FXML
     private TextField locat;
@@ -47,10 +46,10 @@ public class EditAppt {
     private DatePicker startdate;
 
     @FXML
-    private Spinner<?> starttimehour;
+    private ComboBox<String> startHour;
 
     @FXML
-    private Spinner<?> starttimemin;
+    private ComboBox<String> startMinute;
 
     @FXML
     private TextField title;
@@ -60,6 +59,12 @@ public class EditAppt {
 
     @FXML
     private TextField user_id;
+
+    ObservableList<String> hours = FXCollections.observableArrayList();
+    ObservableList<String> minutes = FXCollections.observableArrayList();
+    int u_id;
+    ResourceBundle resourceBundle;
+
 
     @FXML
     void contactID(ActionEvent event) {
@@ -137,6 +142,21 @@ public class EditAppt {
     }
 
     public void initData(Appointment appointment, int id, ResourceBundle rb) {
+
+        u_id = id;
+        resourceBundle = rb;
+
+        hours.setAll("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
+                "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23");
+        minutes.addAll("00", "15", "30", "45");
+
+        startHour.setItems(hours);
+        startMinute.setItems(minutes);
+
+        endHour.setItems(hours);
+        endMinute.setItems(minutes);
+
+
         appt_id.setText(String.valueOf(appointment.getAppointment_ID()));
         title.setText(appointment.getTitle());
         locat.setText(appointment.getLocation());
@@ -147,6 +167,12 @@ public class EditAppt {
         user_id.setText(String.valueOf(appointment.getUser()));
         startdate.setValue(appointment.getStartLocal().toLocalDate());
         end_date.setValue(appointment.getEndLocal().toLocalDate());
+
+
+        startHour.setValue(String.valueOf(appointment.getStartLocal().getHour()));
+        startMinute.setValue(String.valueOf(appointment.getStartLocal().getMinute()));
+        endHour.setValue(String.valueOf(appointment.getEndLocal().getHour()));
+        endMinute.setValue(String.valueOf(appointment.getEndLocal().getMinute()));
 
 
 
